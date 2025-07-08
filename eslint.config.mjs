5464287d9c5a69e14a12802719f9ac7a58017095
Tu Nombre <usuario@ejemplo.com>
2025-07-07 22:50:05 -0300

    Actualizar configuración de ESLint para incluir Prettier, extendiendo las reglas recomendadas y estableciendo errores para el formato de código. Esto mejora la consistencia y calidad del código en el proyecto.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: ['prettier'],
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
 ]);

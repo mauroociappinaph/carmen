e8d726a19845da80c3daed0ba6edfdb2c435f9a9
Tu Nombre <usuario@ejemplo.com>
2025-07-07 22:58:27 -0300

    Agregar middleware CORS en el servidor Express para permitir solicitudes de diferentes orígenes, mejorando la interoperabilidad de la API.

--- Carmen/backend/src/index.js
+++ Carmen/backend/src/index.js
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
 // Importa el barril de rutas
